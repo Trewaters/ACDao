c9ad2f130a593d5815dd487fb5f1cecc3db13431
Berkeley True <berkeleytrue@users.noreply.github.com>
2022-03-09 23:11:04 -0800

    fix: remove unused files

--- index.ts
+++
@@ -1,6 +0,0 @@
-import { TezosToolkit } from "@taquito/taquito";
-import { BeaconWallet } from "@taquito/beacon-wallet";
-
-let Tezos: TezosToolkit;
-const rpcUrl = "https://api.tez.ie/rpc/florencenet";
-
